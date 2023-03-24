5cca42915e36ef239e064fa5010633b30f8888bb
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-03-24 10:36:46 +0700

    menampilkan susunan data array

--- insert/insert.cpp
+++ insert/insert.cpp
@@ -18,4 +18,14 @@ void input() {		//procedure input
 			cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; //menampilkan pesan jika data lebih dari 20
 		}
 	}
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "====================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
 }
