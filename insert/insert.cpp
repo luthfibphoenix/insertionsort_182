b8d3fd001686ba04d746b98b4fb65b89e82cc650
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-03-24 10:26:27 +0700

    prosedur input

--- insert/insert.cpp
+++ insert/insert.cpp
@@ -3,3 +3,10 @@ using namespace std;
 
 int arr[20];		//membuat array dengan panajang data 20
 int n;				//membuat variabel inputan n
+
+void input() {		//procedure input
+	while (true)	
+	{
+		cout << "Masukkan Jumlah Data pada Array : ";	//membuat inputan jumlah elemen array
+	}
+}
